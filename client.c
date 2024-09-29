7edec7967db89fd07d6334aa671ea7f109248df6
Hayk <krest.kb@gmail.com>
2024-09-29 21:19:43 +0400

    bug fix

--- client.c
+++ client.c
@@ -1,6 +1,7 @@
 #include "include.h"
 
 int main(int argc, char* argv[]) {
+
     if (argc < 2) {
         printf("wrong usage: usage ./executable <ip_addres>");
         exit(EXIT_FAILURE);
@@ -25,6 +26,8 @@ int main(int argc, char* argv[]) {
     }
 
     while (1) {
+        fflush(stdout); 
+        fflush(stdin); 
         printf("type request GET POST OPTIONS(OPTIONS 0 PROTOCOL NAME) DELETE or exit\n");
         char buffer[REQUEST_SIZE];
         fgets(buffer, REQUEST_SIZE, stdin);
@@ -45,7 +48,7 @@ int main(int argc, char* argv[]) {
                 fflush(stdout);
                 fflush(stdin); 
 
-                printf("%s",response);
+                printf("%s ",response);
                 if (strcmp(response, "404 error protocol\n") == 0 || 
                     strcmp(response, "SERVER RESPONSE\n404 file not found\n") == 0) {
                     close(sockfd);
@@ -76,10 +79,7 @@ int main(int argc, char* argv[]) {
 
             char success_response[RR];
             int read_bytes = 0;
-            // while ((read_bytes = recv(sockfd, success_response, RR - 1, 0)) > 0) {
-            //     success_response[read_bytes] = '\0';
-            //     printf("%s\n", success_response);
-            // }
+            
 
             char response[NUM];
             while ((read_bytes = recv(sockfd, response, NUM - 1, 0)) > 0) {
@@ -120,4 +120,3 @@ int main(int argc, char* argv[]) {
     close(sockfd);
     return 0;
 }
-
